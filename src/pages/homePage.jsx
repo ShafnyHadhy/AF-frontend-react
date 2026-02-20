2cb353f9e085db0bf457860026ebc64485bf2b51
AgashJeeva <agashbilla2001@gmail.com>
2026-02-20 16:55:41 +0530

    feat: Add AddProductPage and MyProductsPage components with product registration and listing functionality

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Header from "../components/header";
+import MyProductsPage from "./myProductsPage";
 
 export default function HomePage() {
     return (
@@ -13,6 +14,7 @@ export default function HomePage() {
                 <Route path="/about" element={<h1 className="text-3xl font-bold text-primary">About Page</h1>} />
                 <Route path="/contact" element={<h1 className="text-3xl font-bold text-primary">Contact Page</h1>} />
                 <Route path="/*" element={<h1 className="text-3xl font-bold text-primary">404 Not Found</h1>} />
+                <Route path="/my-products" element={<MyProductsPage />} />
             </Routes>
 
         </div>
